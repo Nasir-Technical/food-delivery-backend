64539c896135e80d4148391a16d99c9d93dda68b
Nasir-Technical <nasir.18.2006@gmail.com>
2025-04-22 02:32:55 +0500

    changes

--- server.js
+++ server.js
@@ -58,6 +58,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server started on http://localhost:${port}`);
+// });
+
+export default app;
